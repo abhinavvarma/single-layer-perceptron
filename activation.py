f3d64d561e8d02d45f4da9be9e1c3effc1607972
atipirisetty <atipirisetty@paypal.com>
2017-09-20 12:49:28 -0700

    changed relu and tanh activation funs

--- activation.py
+++ activation.py
@@ -7,18 +7,20 @@ def threshold(k, *args):
     return (0 if k < theta else 1)
 
 
-def tanh(k, *args):
+def logistic(k, *args):
     ''' Tanh activation function '''
-    return math.tanh(k)
+    theta = args[0]
+    return 0.5 + 0.5 * math.tanh((k - theta)/2)
 
 
 def relu(k, *args):
     ''' ReLu activation function '''
-    return max(0, k)
+    theta = args[0]
+    return max(0, k - theta)
 
 
 ALLOWED_TYPES = {
     "threshold": threshold,
-    "tanh": tanh,
+    "tanh": logistic,
     "relu": relu
 }
